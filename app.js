132554b054c0399a6066da2adb9482fb109d9e38
myproject2025 <167327944+myproject2025@users.noreply.github.com>
2024-09-20 14:08:08 +0500

    UPDATE

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 
- // scroll section active link
- let sections = document.querySelectorAll('section')
- let navlinks = document.querySelectorAll('header nav a')
+// // scroll section active link
+// let sections = document.querySelectorAll('section')
+// let navlinks = document.querySelectorAll('header nav a')
 
- window.onscroll = () => {
-     sections.forEach(sec => {
-         let top = window.scrollY
-         let offset = sec.offsetTop - 150
-         let height = sec.offsetHeight
-         let id = sec.getAttribute('id')
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY
+//         let offset = sec.offsetTop - 150
+//         let height = sec.offsetHeight
+//         let id = sec.getAttribute('id')
 
-         if (top >= offset && top < offset + height) {
-             navlinks.forEach(links => {
-                 links.classList.remove('active')
-                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-             })
-
-
-         }
-     })
-     // Sticky navber
-     let header = document.querySelector('header')
-     header.classList.toggle('sticky', window.scrollY > 100)
-
-     menuIcon.classList.remove('bx-x')
-     navbar.classList.remove('active')
- }
+//         if (top >= offset && top < offset + height) {
+//             navlinks.forEach(links => {
+//                 links.classList.remove('active')
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+//             })
 
 
- ScrollReveal({
-      reset: true,
-     distance: '80px',
-     duration: 2000,
-     delay: 200
- });
+//         }
+//     })
+//     // Sticky navber
+//     let header = document.querySelector('header')
+//     header.classList.toggle('sticky', window.scrollY > 100)
 
- ScrollReveal().reveal('.header, .project-card , #sub', { origin: 'top' });
- ScrollReveal().reveal('.section-tittle , .section-tittle', { origin: 'bottom' });
- ScrollReveal().reveal('.about img ,  #gridimg , .input', { origin: 'left' });
- ScrollReveal().reveal('.text, .btn-group , .socials , .grid ', { origin: 'right' });
+//     menuIcon.classList.remove('bx-x')
+//     navbar.classList.remove('active')
+// }
+
+
+// ScrollReveal({
+//      reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200
+// });
+
+// ScrollReveal().reveal(' .project-card , #sub', { origin: 'top' });
+// ScrollReveal().reveal('.section-tittle , .section-tittle', { origin: 'bottom' });
+// ScrollReveal().reveal('.about img ,  #gridimg , .input', { origin: 'left' });
+// ScrollReveal().reveal('.text, .btn-group , .socials , .grid ', { origin: 'right' });
 
 
 
@@ -46,12 +46,12 @@
  
 
 
- const typed = new Typed('.multiple-text', {
- strings: ['Frontend Developer'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+const typed = new Typed('.multiple-text',{
+strings: ['Frontend Developer'],
+   typeSpeed: 100,
+   backSpeed: 100,
+   backDelay: 1000,
+   loop: true
 })
 
 
